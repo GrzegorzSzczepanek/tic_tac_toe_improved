6b6cb1b63ff7fd449f06e4aa7ad135b43510eb05
Grzesiek <grzegorzszczepanek48@gmail.com>
2023-08-25 00:36:47 +0200

    Work in progress
    Still need to figure out sockets and connection between two separate programs with games,

--- tic_tac_toe_improved/main.py
+++ tic_tac_toe_improved/main.py
@@ -1,19 +1,59 @@
 import socket
+import threading
+
+HEADER = 64 # bytes
+PORT = 5050
+SERVER = "192.168.0.108"
+#SERVER = socket.gethostbyname(socket.gethostname())
+# print(SERVER)
+# print(socket.gethostname())
+# print(socket.gethostbyaddr("192.168.0.189"))
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+ADDR = (SERVER, PORT)
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(("127.0.0.1", 15200))
-server.listen(1)
-
-print("Server started!")
-
-# accept incoming connections
-client_socket, client_address = server.accept()
-print("Accepted connection from ", client_address)
+server.connect(ADDR)
 
 
-while True:
-    data = client_socket.recv(2048)
-    data = data.decode('utf-8')
-    print("Received: ", data)
+messages = []
+connections = []
 
-    client_socket.send(data.encode())
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected")
+    connections.append(addr)
+    connected = True
+
+    message_length = conn.recv(HEADER).decode(FORMAT)
+    if message_length:
+        message_length = int(message_length)
+        message = conn.recv(message_length).decode(FORMAT)
+        messages.append(message)
+
+        # print(f"[{addr}] {message}")
+        for i in messages:
+            #if addr == i:
+            conn.send(f"{i}\n".encode(FORMAT))
+
+        print(messages, connected, connections, sep="\n")
+    conn.close()
+
+
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        # When the new connection occurs we store IP adress of that connection
+        # and port and object that's going to allow us to communicate back
+        conn, addr = server.accept()
+        # When a new connection occurs we pass that connection to handle clients
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CPNNECTIONS] {threading.active_count() - 1}")
+
+
+print("[STARTING] server is starting...")
+start()
+
+
+

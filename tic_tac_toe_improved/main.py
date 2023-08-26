994f805461d6878328de8ab50f9510bb618c2077
Grzesiek <grzegorzszczepanek48@gmail.com>
2023-08-26 13:59:15 +0200

    Update main.py

--- tic_tac_toe_improved/main.py
+++ tic_tac_toe_improved/main.py
@@ -1,59 +1,65 @@
 import socket
 import threading
 
-HEADER = 64 # bytes
+# Connection Data
+HOST = '192.168.0.189'
 PORT = 5050
-SERVER = "192.168.0.108"
-#SERVER = socket.gethostbyname(socket.gethostname())
-# print(SERVER)
-# print(socket.gethostname())
-# print(socket.gethostbyaddr("192.168.0.189"))
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
-ADDR = (SERVER, PORT)
-
+ADDR = (HOST, PORT)
+# Starting Server
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.connect(ADDR)
+server.bind(ADDR)
+server.listen()
+
+# Lists For Clients and Their Nicknames
+clients = []
+# nicknames = []
 
 
-messages = []
-connections = []
-
-def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected")
-    connections.append(addr)
-    connected = True
-
-    message_length = conn.recv(HEADER).decode(FORMAT)
-    if message_length:
-        message_length = int(message_length)
-        message = conn.recv(message_length).decode(FORMAT)
-        messages.append(message)
-
-        # print(f"[{addr}] {message}")
-        for i in messages:
-            #if addr == i:
-            conn.send(f"{i}\n".encode(FORMAT))
-
-        print(messages, connected, connections, sep="\n")
-    conn.close()
+def broadcast(message):
+    print(f"--------------\n{message}\n----------------")
+    for client in clients:
+        client.send(message)
 
 
-def start():
-    server.listen()
-    print(f"[LISTENING] Server is listening on {SERVER}")
+def handle(client):
+    print(f"--------------\n{client}\n----------------")
     while True:
-        # When the new connection occurs we store IP adress of that connection
-        # and port and object that's going to allow us to communicate back
-        conn, addr = server.accept()
-        # When a new connection occurs we pass that connection to handle clients
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        try:
+            # Broadcasting Messages
+            message = client.recv(1024)
+            print(f"--------------\n{message}\n----------------".encode('ascii'))
+            broadcast(message)
+        except:
+            # Removing And Closing Clients
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            #nickname = nicknames[index]
+            #broadcast('{} left!'.format(nickname).encode('ascii'))
+            #nicknames.remove(nickname)
+            break
+
+
+def receive():
+    while True:
+        # Accept Connection
+        client, address = server.accept()
+        print("Connected with {}".format(str(address)))
+
+        # Request And Store Nickname
+        # client.send('NICK'.encode('ascii'))
+        # nickname = client.recv(1024).decode('ascii')
+        # nicknames.append(nickname)
+        clients.append(client)
+        #print(clients)
+        # Print And Broadcast Nickname
+        # print("Nickname is {}".format(nickname))
+        # broadcast("{} joined!".format(nickname).encode('ascii'))
+        # client.send('Connected to server!'.encode('ascii'))
+        # Start Handling Thread For Client
+        thread = threading.Thread(target=handle, args=(client,))
         thread.start()
-        print(f"[ACTIVE CPNNECTIONS] {threading.active_count() - 1}")
 
 
-print("[STARTING] server is starting...")
-start()
-
-
-
+print(f"Server is listening on [{ADDR}]")
+receive()
